4c3db6400190f45c1de16910df7d2d08e8b741e0
Kiran kumar Matam <matamkiran@users.noreply.github.com>
2023-07-06 08:24:51 +0530

    commiting elastic search repo

---
+++ SpringBootElasticsearchExample-master/src/main/java/com/example/springbootelasticsearchexample/entity/Product.java
@@ -0,0 +1,61 @@
+package com.example.springbootelasticsearchexample.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document(indexName = "products")
+public class Product {
+   private int id;
+    private String name;
+    private String description;
+
+    private int quantity;
+
+    private double price;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+    
+}
